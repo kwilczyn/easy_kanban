04b85e10a5233c85cacfab86e6290a69db05ebdd
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-10-25 11:55:47 +0200

    chore: use api proxy server for dev mode

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ command, mode }) => {
+  const isDev = command === 'serve' // Sprawdza, czy jest to tryb deweloperski
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    // Tylko w trybie deweloperskim dodajemy konfigurację proxy
+    ...(isDev && {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8000/api',
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, '')
+          }
+        }
+      }
+    })
   }
 })
