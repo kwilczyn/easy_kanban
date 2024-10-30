9255e714e7bc48490c78ad1c183f0262327c59fd
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-10-30 11:07:27 +0100

    test(e2e): use more specific selectors for lists and tasks

--- e2e/pages/components/task.js
+++ e2e/pages/components/task.js
@@ -1,5 +1,10 @@
 export default function taskByTitle(list, title) {
-  const task = list.locator('li', { hasText: title })
+  let task = null
+  if (title) {
+    task = list.locator(`li:has(:text-is("${title}"))`)
+  } else {
+    task = list.locator('li:has([draggable])')
+  }
   task.burgerMenuButton = task.getByRole('button', { name: '...' })
   return task
 }
