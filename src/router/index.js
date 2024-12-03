570b67d4f16bb8d1c107b4d590f3c21ef0c5f3e7
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-12-03 13:24:41 +0100

    fix(router): remove async components from the router
    It turned out that async components are not recommended for routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { defineAsyncComponent } from 'vue'
 
-const HomeView = defineAsyncComponent(() => import('../views/HomeView.vue'))
-const LoginView = defineAsyncComponent(() => import('../views/LoginView.vue'))
+const HomeView = import('../views/HomeView.vue')
+const LoginView = import('../views/LoginView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
